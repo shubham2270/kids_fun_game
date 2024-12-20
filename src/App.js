52318f5eebbe553f537b06e44b111983927ac7d6
Shubham Kumar <shubham2270@gmail.com>
2024-12-20 17:08:05 +0530

    add shape components and update game logic for matching

--- src/App.js
+++ src/App.js
@@ -8,23 +8,24 @@ import restartSound from "./sounds/restart-sound.mp3";
 import bgSound from "./sounds/bg-music.mp3";
 
 const shapes = [
-  { id: 1, name: "Circle", color: "red" },
-  { id: 2, name: "Square", color: "blue" },
-  { id: 3, name: "Triangle", color: "green" },
-  { id: 4, name: "Star", color: "yellow" },
+  { id: 1, name: "circle" },
+  { id: 2, name: "square" },
+  { id: 3, name: "triangle" },
+  { id: 4, name: "star" },
 ];
 
 const targets = [
-  { id: 1, name: "Circle", color: "red" },
-  { id: 2, name: "Square", color: "blue" },
-  { id: 3, name: "Triangle", color: "green" },
-  { id: 4, name: "Star", color: "yellow" },
+  { id: 1, name: "circle", isMatched: false },
+  { id: 2, name: "square", isMatched: false },
+  { id: 3, name: "triangle", isMatched: false },
+  { id: 4, name: "star", isMatched: false },
 ];
 
 function App() {
   const [score, setScore] = useState(0);
   const [dragging, setDragging] = useState(null);
   const [isGameStarted, setIsGameStarted] = useState(false);
+  const [currentTargets, setCurrentTargets] = useState(targets);
   const soundRef = React.useRef(false);
 
   const playSound = (sound) => {
@@ -48,6 +49,14 @@ function App() {
     const draggedShape = shapes.find((shape) => shape.id === dragging);
     if (dragging === id) {
       setScore(score + 1);
+      setCurrentTargets(
+        [...currentTargets].map((target) => {
+          if (target.id === id) {
+            return { ...target, isMatched: true };
+          }
+          return target;
+        })
+      );
       playSound(correctSound);
     } else {
       playSound(wrongSound);
@@ -90,22 +99,24 @@ function App() {
                   className='shape'
                   draggable
                   onDragStart={() => handleDragStart(shape.id)}
-                  style={{ backgroundColor: shape.color }}
                 >
-                  {shape.name}
+                  <div class={shape.name}></div>
                 </div>
               ))}
             </div>
             <div className='targets'>
-              {targets.map((target) => (
+              {currentTargets.map((target) => (
                 <div
                   key={target.id}
                   className='target'
                   onDragOver={(e) => e.preventDefault()}
                   onDrop={() => handleDrop(target.id)}
-                  style={{ backgroundColor: target.color }}
                 >
-                  Drop Here
+                  {target.isMatched ? (
+                    <div class={target.name}></div>
+                  ) : (
+                    <div class={`empty-${target.name}`}></div>
+                  )}
                 </div>
               ))}
             </div>
